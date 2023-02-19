a2843e89f13030b3ced478e9976e8cfecb8e4ed0
Mykhailo Babiuk <master7dok@gmail.com>
2023-02-19 21:36:39 +0100

    added xml configuration with tomcat

---
+++ src/main/java/com/mbdev/springmvcalishev/HelloController.java
@@ -0,0 +1,13 @@
+package com.mbdev.springmvcalishev;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello-world")
+    public String hello(){
+        return "hello_world";
+    }
+}
